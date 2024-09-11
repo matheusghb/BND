55a071f47f920dddea290af891b8f55494e91c67
matheusghb <161875342+matheusghb@users.noreply.github.com>
2024-09-11 17:41:17 -0300

    Add files via upload

--- aula.js
+++ aula.js
@@ -1,10 +1,13 @@
-function calculos(a, b) {
-    a = 8
-    b = 3
-    let abso = a+b
-    let absu = a-b
-    let abm = a*b
-    let abd = a/b
-    console.log(abso, absu, abm, abd)
+function calculos() {
+    a = Number(document.getElementById("a").value);
+    b = Number(document.getElementById("b").value);
+    parseFloat(a);
+    parseFloat(b);
+    console.log(a, b);
+    let abso = (a) + (b);
+    let absu = a-b;
+    let abm = a*b;
+    let abd = a/b;
+    console.log(abso, absu, abm, abd);
 }
 
