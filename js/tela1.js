efb49562e3ad264b7def0f4ce595cc47e02d451d
matheusghb <mistermquack124@gmail.com>
2024-09-12 17:27:15 -0300

    adição de banco de dados

---
+++ js/tela1.js
@@ -0,0 +1,5 @@
+function login() {
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+
+}
---
+++ js/tela2.js
@@ -0,0 +1,5 @@
+function registr() {
+    let username = document.getElementById("username").value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+}
