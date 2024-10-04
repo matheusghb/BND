2a881a0e34ced7f90013fd128f2658a2ef2975e6
PedroLucaspllgc <161877989+PedroLucaspllgc@users.noreply.github.com>
2024-10-04 11:41:52 -0300

    Update temaescuro.js

--- js/temaescuro.js
+++ js/temaescuro.js
@@ -1,10 +1,18 @@
 const darkModeToggle = document.getElementById('input-dark-mode');
 const bodyElement = document.body;
 
+// Check if dark mode is already enabled
+if (localStorage.getItem('darkMode') === 'true') {
+  bodyElement.classList.add('dark-mode');
+  darkModeToggle.checked = true;
+}
+
 darkModeToggle.addEventListener('change', () => {
   if (darkModeToggle.checked) {
     bodyElement.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'true');
   } else {
     bodyElement.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'false');
   }
 });
