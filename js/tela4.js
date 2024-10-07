0704f2382f91fac5bd3eff31c3455e9f77c24273
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2024-10-07 17:38:16 -0300

    Update tela4.js

--- js/tela4.js
+++ js/tela4.js
@@ -1,3 +1,20 @@
+document.getElementById('colorInput').addEventListener('input', function() {
+    const color = this.value;
+    
+    // Seleciona os slots
+    const slots = [
+        document.querySelector('.slot1'),
+        document.querySelector('.slot2'),
+        document.querySelector('.slot3'),
+        document.querySelector('.slot4'),
+        document.querySelector('.slot5')
+    ];
+
+    // Altera a cor de cada slot
+    slots.forEach(slot => {
+        slot.style.backgroundColor = color;
+    });
+});
 function adcor () {
     
 }
