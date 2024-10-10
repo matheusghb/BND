3d70799d476f29df5303fddfeb50206336dbea62
PedroLucaspllgc <161877989+PedroLucaspllgc@users.noreply.github.com>
2024-10-10 17:13:52 -0300

    Update tela4.js

--- js/tela4.js
+++ js/tela4.js
@@ -7,6 +7,20 @@ slotcor.onchange = function() {
 var slots = document.querySelectorAll('.slot');
 var inputs = document.querySelectorAll('input[type="color"]');
 
+const fileInput = document.getElementById('fileInput');
+const preview = document.getElementById('preview');
+
+fileInput.onchange = function() {
+  const fileName = fileInput.files[0].name;
+  fileInput.placeholder = fileName;
+
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    preview.src = event.target.result;
+  };
+  reader.readAsDataURL(fileInput.files[0]);
+}
+
 inputs.forEach((input, index) => {
   input.onchange = function() {
     slots[index].style.backgroundColor = input.value;
