f620d494b03d73c204addb19a08995591ab68550
PedroLucaspllgc <161877989+PedroLucaspllgc@users.noreply.github.com>
2024-10-12 12:31:35 -0300

    Update tela5.js

--- js/tela5.js
+++ js/tela5.js
@@ -1,61 +1,61 @@
 const colorBox = document.getElementById('color-box');
 const circle = document.getElementById('circle');
-const circleInner = circle.querySelector('div'); // Get the child div element
+const circleInner = circle.querySelector('div');
 const colorInput = document.getElementById('color-input');
 
 circle.addEventListener('click', (event) => {
-  event.stopPropagation(); // Prevent the event from bubbling up
+  event.stopPropagation(); 
   colorInput.click();
 });
 
 colorInput.addEventListener('input', () => {
   const selectedColor = colorInput.value;
-  circleInner.style.backgroundColor = selectedColor; // Set the background color of the child div element
+  circleInner.style.backgroundColor = selectedColor;
 });
 
 const colorBox2 = document.getElementById('color-box-2');
 const circle2 = document.getElementById('circle-2');
-const circleInner2 = circle2.querySelector('div'); // Get the child div element
+const circleInner2 = circle2.querySelector('div');
 const colorInput2 = document.getElementById('color-input-2');
 
 circle2.addEventListener('click', (event) => {
-  event.stopPropagation(); // Prevent the event from bubbling up
+  event.stopPropagation(); 
   colorInput2.click();
 });
 
 colorInput2.addEventListener('input', () => {
   const selectedColor = colorInput2.value;
-  circleInner2.style.backgroundColor = selectedColor; // Set the background color of the child div element
+  circleInner2.style.backgroundColor = selectedColor; 
 });
 
 const colorBox3 = document.getElementById('color-box-3');
 const circle3 = document.getElementById('circle-3');
-const circleInner3 = circle3.querySelector('div'); // Get the child div element
+const circleInner3 = circle3.querySelector('div');
 const colorInput3 = document.getElementById('color-input-3');
 
 circle3.addEventListener('click', (event) => {
-  event.stopPropagation(); // Prevent the event from bubbling up
+  event.stopPropagation();
   colorInput3.click();
 });
 
 colorInput3.addEventListener('input', () => {
   const selectedColor = colorInput3.value;
-  circleInner3.style.backgroundColor = selectedColor; // Set the background color of the child div element
+  circleInner3.style.backgroundColor = selectedColor;
 });
 
 const colorBox4 = document.getElementById('color-box-4');
 const circle4 = document.getElementById('circle-4');
-const circleInner4 = circle4.querySelector('div'); // Get the child div element
+const circleInner4 = circle4.querySelector('div');
 const colorInput4 = document.getElementById('color-input-4');
 
 circle4.addEventListener('click', (event) => {
-  event.stopPropagation(); // Prevent the event from bubbling up
+  event.stopPropagation();
   colorInput4.click();
 });
 
 colorInput4.addEventListener('input', () => {
   const selectedColor = colorInput4.value;
-  circleInner4.style.backgroundColor = selectedColor; // Set the background color of the child div element
+  circleInner4.style.backgroundColor = selectedColor;
 });
 
 const canvas = document.getElementById('canvas');
@@ -94,34 +94,4 @@ canvas.addEventListener('mouseup', () => {
 
 canvas.addEventListener('mouseout', () => {
   isDrawing = false;
-});
-
-const colorBox3 = document.getElementById('color-box-3');
-const circle3 = document.getElementById('circle-3');
-const circleInner3 = circle3.querySelector('div'); // Get the child div element
-const colorInput3 = document.getElementById('color-input-3');
-
-circle3.addEventListener('click', (event) => {
-  event.stopPropagation(); // Prevent the event from bubbling up
-  colorInput3.click();
-});
-
-colorInput3.addEventListener('input', () => {
-  const selectedColor = colorInput3.value;
-  circleInner3.style.backgroundColor = selectedColor; // Set the background color of the child div element
-});
-
-const colorBox4 = document.getElementById('color-box-4');
-const circle4 = document.getElementById('circle-4');
-const circleInner4 = circle4.querySelector('div'); // Get the child div element
-const colorInput4 = document.getElementById('color-input-4');
-
-circle4.addEventListener('click', (event) => {
-  event.stopPropagation(); // Prevent the event from bubbling up
-  colorInput4.click();
-});
-
-colorInput4.addEventListener('input', () => {
-  const selectedColor = colorInput4.value;
-  circleInner4.style.backgroundColor = selectedColor; // Set the background color of the child div element
 });
